6ac7f8a61d0c9e7ff28c29952ecebd3889e2e7e2
estis135 <estevan.santos@gmail.com>
2021-04-05 14:05:27 -0300

    alteracao

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3001
 app.get('/', (req,res)=>{
-  res.send('servidor pronto')
+  res.send('servidor pronto ok w')
 })
 app.get('/api/products', (req, res) => {
   res.send([
