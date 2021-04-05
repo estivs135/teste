d7e1196b9c8e82c4dd72d97ec6e698ae38f96c74
estis135 <estevan.santos@gmail.com>
2021-04-05 19:39:42 -0300

    outra

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3001
 app.get('/', (req,res)=>{
-  res.send('servidor pronto ok w')
+  res.send('servidor pronto ok')
 })
 app.get('/api/products', (req, res) => {
   res.send([
