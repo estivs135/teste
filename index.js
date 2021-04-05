04e2d4a91d2fab54d96cff2c46d88e6a8260c21b
estis135 <estevan.santos@gmail.com>
2021-04-05 18:39:23 -0300

    alter

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.get('/api/products', (req, res) => {
   res.send([
       {
         _id: '1',
-        name: 'Nike Slim Shirt',
+        name: 'Nike Slim Shirto',
         category: 'Shirts',
         image: '/images/p1.jpg',
         price: 120,
